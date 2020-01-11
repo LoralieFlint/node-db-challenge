e5eda024c5250a6cd19c98d5a45c734640537a75
ittyBittyBytes <loralie.flint0128@gmail.com>
2020-01-11 10:30:57 -0700

    added resource table working in dbBrowser

--- migrations/20200111101819_projects.js
+++ migrations/20200111101819_projects.js
@@ -1,12 +1,22 @@
 exports.up = async function(knex) {
-  await knex.schema.createTable("projects", table => {
-    table.increments("id");
-    table.string("name").notNullable();
-    table.string("description");
-    table.boolean("completed");
-  });
+  await knex.schema
+    .createTable("projects", table => {
+      table.increments("id");
+      table.string("project_name").notNullable();
+      table.string("project_description");
+      table.boolean("completed").defaultTo(false);
+    })
+    .createTable("resources", table => {
+      table.increments();
+      table
+        .string("resource_name", 128)
+        .notNullable()
+        .unique();
+      table.text("resource_description");
+    });
 };
 
 exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("resources");
   await knex.schema.dropTableIfExists("projects");
 };
