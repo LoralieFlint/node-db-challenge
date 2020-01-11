b476f146d3d7bfbfa8d3abcff399ca5792c35718
ittyBittyBytes <loralie.flint0128@gmail.com>
2020-01-11 10:40:51 -0700

    tasks migration added and works in dbBrowser

--- migrations/20200111101819_projects.js
+++ migrations/20200111101819_projects.js
@@ -7,16 +7,54 @@ exports.up = async function(knex) {
       table.boolean("completed").defaultTo(false);
     })
     .createTable("resources", table => {
-      table.increments();
+      table.increments("id");
       table
         .string("resource_name", 128)
         .notNullable()
         .unique();
       table.text("resource_description");
+    })
+    .createTable("projects_resources", table => {
+      table.increments("id");
+      table
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("projects")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      table
+        .integer("resource_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("resources")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    })
+    .createTable("tasks", table => {
+      table.increments("id");
+      table.text("task_description").notNullable();
+      table.text("notes");
+      table
+        .boolean("completed")
+        .default(false)
+        .notNullable();
+      table
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("projects")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
     });
 };
 
 exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("tasks");
+  await knex.schema.dropTableIfExists("projects_resources");
   await knex.schema.dropTableIfExists("resources");
   await knex.schema.dropTableIfExists("projects");
 };
